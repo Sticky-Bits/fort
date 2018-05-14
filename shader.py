1fc97d0fe6ca9580a65b2f20449a6431ad9f0601
Dean Mckendry <dean.mckendry91@gmail.com>
2018-05-14 17:28:53 +1000

    Rework shader class uniform methods

--- shader.py
+++ shader.py
@@ -1,5 +1,19 @@
-from pyglet.gl import glUseProgram, GL_VERTEX_SHADER, GL_FRAGMENT_SHADER
+from pyglet.gl import (GL_FALSE, GL_FRAGMENT_SHADER, GL_VERTEX_SHADER,
+                       glAttachShader, glCompileShader, glCreateProgram,
+                       glCreateShader, glDeleteShader, glGetUniformLocation,
+                       glLinkProgram, glShaderSource, glUniform1f, glUniform1i,
+                       glUniform3fv, glUniform4fv, glUniformMatrix3fv,
+                       glUniformMatrix4fv, glUseProgram)
+from pyrr.objects import Matrix33, Matrix44, Vector3, Vector4
+from ctypes import c_float
+
 from OpenGL.GL.shaders import compileProgram, compileShader
+
+# Define types as constants in case we need to change libraries later
+VECTOR3_TYPE = Vector3
+VECTOR4_TYPE = Vector4
+MATRIX3_TYPE = Matrix33
+MATRIX4_TYPE = Matrix44
 
 
 class Shader:
@@ -11,12 +25,12 @@ class Shader:
         with open(vertex_path) as f:
             self.vertex_shader_source = f.readlines()
         with open(fragment_path) as f:
-            self.fragment_shader_source = f.readlines()
+            self.fragment_shader_source = "".join(f.readlines())
         if geometry_path:
             raise NotImplementedError
             # with open(geometry_path) as f:
             #     self.geometry_shader_source = f.readlines()
-
+        # use pyopengl shortcuts for now, not sure if I need to do this manually
         self.shader = compileProgram(compileShader(self.vertex_shader_source, GL_VERTEX_SHADER),
                                      compileShader(self.fragment_shader_source, GL_FRAGMENT_SHADER))
 
@@ -24,26 +38,23 @@ class Shader:
         glUseProgram(self.shader)
 
     # Utility uniform functions
-    def set_bool(self):
-        raise NotImplementedError
+    def set_uniform(self, name, value):
+        name = name.encode()
+        valtype = type(value)
+        if valtype == bool or valtype == int:
+            glUniform1i(glGetUniformLocation(self.shader, name), int(value))
+        elif valtype == float:
+            glUniform1f(glGetUniformLocation(self.shader, name), value)
+        # elif valtype == VECTOR2_TYPE:
+        #     glUniform2fv(glGetUniformLocation(self.shader, name), 1, value)
+        elif valtype == VECTOR3_TYPE:
+            glUniform3fv(glGetUniformLocation(self.shader, name), 1, c_float(value[0]), c_float(value[1]), c_float(value[2]))
+        elif valtype == VECTOR4_TYPE:
+            glUniform4fv(glGetUniformLocation(self.shader, name), 1, c_float(value[0]), c_float(value[1]), c_float(value[2]), c_float(value[3]))
+        elif valtype == MATRIX3_TYPE:
+            glUniformMatrix3fv(glGetUniformLocation(self.shader, name), 1, GL_FALSE, c_float(value[0][0]))  # Should this be value[0][0]?
+        elif valtype == MATRIX4_TYPE:
+            glUniformMatrix4fv(glGetUniformLocation(self.shader, name), 1, GL_FALSE, c_float(value[0][0]))  # Should this be value[0][0]?
 
-    def set_int(self):
-        raise NotImplementedError
-
-    def set_float(self):
-        raise NotImplementedError
-
-    def set_vec2(self):
-        raise NotImplementedError
-
-    def set_vec3(self):
-        raise NotImplementedError
-
-    def set_vec4(self):
-        raise NotImplementedError
-
-    def set_mat3(self):
-        raise NotImplementedError
-
-    def set_mat4(self):
-        raise NotImplementedError
+    def check_compile_errors(self):
+        pass
