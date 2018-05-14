3060f4133080d11447c57674617bd5450050319e
Dean Mckendry <dean.mckendry91@gmail.com>
2018-05-14 17:27:42 +1000

    Add shaders with matrix support

--- frag.glsl
+++ frag.glsl
@@ -1,7 +1,48 @@
-#version 330
-in vec3 newColor;
+#version 330 core
+out vec4 FragColor;
 
-out vec4 outColor;
-void main(){
-    outColor = vec4(newColor, 1.0f);
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;    
+    float shininess;
+}; 
+
+struct Light {
+    //vec3 position;
+    vec3 direction;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+in vec3 FragPos;  
+in vec3 Normal;  
+in vec2 TexCoords;
+  
+uniform vec3 viewPos;
+uniform Material material;
+uniform Light light;
+
+void main()
+{
+    // ambient
+    vec3 ambient = light.ambient;// * texture(material.diffuse, TexCoords).rgb;
+  	
+    // diffuse 
+    vec3 norm = normalize(Normal);
+    // vec3 lightDir = normalize(light.position - FragPos);
+    vec3 lightDir = normalize(-light.direction);  
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * diff;// * texture(material.diffuse, TexCoords).rgb;  
+    
+    // specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;  
+        
+    vec3 result = ambient + diffuse;// + specular;
+    //FragColor = vec4(result, 1.0);
+    FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
--- vert.glsl
+++ vert.glsl
@@ -1,10 +1,21 @@
-#version 330
-in layout (location = 0) vec3 position;
-in layout (location = 1) vec3 color;
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
 
-out vec3 newColor;
+out vec3 FragPos;
+out vec3 Normal;
+out vec2 TexCoords;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main()
 {
-    gl_Position = vec4(position, 1.0f);
-    newColor = color;
+    FragPos = vec3(model * vec4(aPos, 1.0));
+    Normal = mat3(transpose(inverse(model))) * aNormal;  
+    TexCoords = aTexCoords;
+    
+    gl_Position = projection * view * vec4(FragPos, 1.0);
 }
